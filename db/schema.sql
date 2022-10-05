fb216451cf7953c8d1cf21f66c18940c16c6cd4c
Jared Love <lovejared91@yahoo.com>
2022-10-05 08:33:59 -0500

    added code to db,schema, and seeds sql to form database

--- db/schema.sql
+++ db/schema.sql
@@ -3,30 +3,25 @@ CREATE TABLE employees{
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(40) NOT NULL,
-    job_title INTEGER,
-    departments INTEGER,
-    salaries INTEGER,
-    manager INTEGER,
-    CONSTRAINT fk_title FOREIGN KEY (job_title) REFERENCES roles(job_title) ON DELETE SET NULL,
-    CONSTRAINT fk_salaries FOREIGN KEY (salaries) REFERENCES roles(salary) ON DELETE SET NULL,
-    CONSTRAINT fk_manager FOREIGN KEY (manager) REFERENCES managers(manager_name) ON DELETE SET NULL
+    role_id INTEGER,
+    department_id INTEGER,
+    salary_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_title FOREIGN KEY (role_id) REFERENCES roles(job_title) ON DELETE SET NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL.
+    CONSTRAINT fk_salaries FOREIGN KEY (salaries_id) REFERENCES roles(salary) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 };
-
-DROP TABLE IF EXISTS managers;
-CREATE TABLE managers {
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    manager_name VARCHAR (40) NOT NULL
-};
-
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles {
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(50) NOT NULL,
-    department INTEGER,
-    salary VARCHAR(30)
+    job_title VARCHAR(30) NOT NULL,
+    department_id INTEGER,
+    salary DECIMAL, 
+    
+    CONSTRAINT fk_department FOREIGN KEY (department) REFERENCES departments(id) ON DELETE SET NULL
 };
-
 
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments {
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO employees(first_name, last_name, role_id, department_id, salary_id, manager_id)
+VALUES
+('Candy', 'Man', 1, 1, 1, NULL),
+('Pin', 'Head', 2, 1, 2, 1),
+('Jason', 'Voorhees', 3, 2, 3, NULL),
+('Jack', 'Torrance', 4, 2, 4, 3),
+('Carrie', 'White', 5, 3, 5, NULL),
+('Ghost', 'Face', 6, 3, 6, 5),
+('Slender', 'Man', 7, 4, 7, NULL),
+('Penny', 'Wise', 8, 4, 8, 7);
+
+
+INSERT INTO roles(job_title, department, salary)
+VALUES 
+('Sales Lead', 1, 100000)
+('Salesperson', 1, 80000)
+('Lead Engineer', 2, 150000)
+('Software Engineer', 2, 120000)
+('Account Manager', 3, 160000)
+('Accountant', 3, 125000)
+('Legal Team Lead', 4, 250000)
+('Lawyer', 4, 190000);
+
+INSERT INTO departments(department_name)
+VALUES
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
